86ca3292791c4190e513e77f9639dd92222365dd
IvanM <ivan.malyshev.376@gmail.com>
2021-01-10 00:08:25 +0300

    InlineChunkHtmlPlugin update

--- webpack.config.js
+++ webpack.config.js
@@ -7,24 +7,27 @@ console.log(`--- Webpack mode:${process.env.NODE_ENV} ---`);
 const mode = process.env.NODE_ENV;
 const config = {
   mode,
-  entry: './src/client.js',
-  output: { filename: 'client-bundle.js' },
+  entry: ['./src/style.sass', './src/client.js'],
+  output: {
+    filename: 'client-bundle.js',
+  },
   plugins: [
-    new HtmlWebpackPlugin({ template: './src/index-template.html' }),
-    new InlineChunkHtmlPlugin(HtmlWebpackPlugin, [/client-bundle/]),
+    new HtmlWebpackPlugin({
+      template: './src/index-template.html',
+    }),
   ],
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: [
-          'style-loader', // Creates `style` nodes from JS strings
-          'css-loader', // Translates CSS into CommonJS
-          // 'sass-loader', // Compiles Sass to CSS
-        ],
+        test: /\.sass$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
     ],
   },
 };
+
+if (mode === 'production') {
+  config.plugins.push(new InlineChunkHtmlPlugin(HtmlWebpackPlugin, [/client-bundle/]));
+}
 
 module.exports = config;
