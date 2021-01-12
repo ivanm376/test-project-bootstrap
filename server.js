3e96c10ecfbc0fb944a1feaa5a927fbbc94060f4
IvanM <ivan.malyshev.376@gmail.com>
2021-01-12 11:21:03 +0300

    remove unused 2

--- server.js
+++ server.js
@@ -78,3 +78,19 @@ app.use(express.static('public'))
 app.listen(process.env.PORT, () => {
   logger.info(`APP listening ENV:${process.env.NODE_ENV} PORT:${process.env.PORT}`)
 })
+
+// const fs = require('fs')
+// const ytdl = require('ytdl-core') // mobile 800x480
+//
+// try {
+//   // const info = await ytdl.getBasicInfo(videoId);
+//   // console.log(info.formats.map(i => `${i.itag} ${i.width}x${i.height}`));
+//   ytdl(`http://www.youtube.com/watch?v=${process.argv[2]}`, {
+//     // quality: 'highest'
+//     quality: 18, // second
+//     //,quality: 'highestaudio',
+//     //,filter: i => i.qualityLabel === '480p' && i.mimeType.slice(0, 10) === "video/webm"
+//   }).pipe(fs.createWriteStream(`${process.argv[3]}.mp4`))
+// } catch (err) {
+//   logger.error(`ytdl_catch ${err}`)
+// }
